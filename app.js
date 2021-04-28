9d00278097431bb5fce91ca801cd67f40ebc9303
FrancoSpinelli <franco_spinelli@hotmail.com>
2021-04-27 21:15:30 -0300

    animaciones

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require ('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(3030,()=> console.log('Servidor corriendo...'));
+app.listen(3030,()=> console.log('Servidor corriendo en el puerto 3030'));
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
