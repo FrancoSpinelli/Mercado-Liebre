0f54a5cc22bba66d45255595d12fd6d58401aa67
FrancoSpinelli <franco_spinelli@hotmail.com>
2021-05-02 15:50:30 -0300

    cambio de configuraciones

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const path = require ('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT || 3030, ()=> console.log('Servidor corriendo en el puerto 3030'));
+app.listen(process.env.PORT || 3030, function(){
+    console.log('Servidor corriendo en el puerto 3030')
+});
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
