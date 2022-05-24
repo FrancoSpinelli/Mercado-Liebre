07c5d08127d114e8b2b87f85c2337fbf711ffffd
FrancoSpinelli <81250647+FrancoSpinelli@users.noreply.github.com>
2022-05-23 23:13:39 -0300

    arreglo header

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "12345678",
     "database": "mercado_liebre",
     "host": "127.0.0.1",
     "dialect": "mysql"
